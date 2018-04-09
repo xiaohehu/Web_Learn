5889d23b57c65e54713aa129a15a1c513834e459
Xiaohe Hu <huxiaohe113@gmail.com>
2018-04-09 01:33:21 -0400

    Added mongoDB feature

---
+++ week2/oj-server/server.js
@@ -0,0 +1,15 @@
+const express = require('express'); // import express package
+const app = express(); // Create http application
+const restRouter = require('./routes/rest'); // import rest router;
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://test:test@ds147069.mlab.com:47069/problems');
+
+// if the url matches 'api/v1', it will use restRouter to handele the traffic
+app.use('/api/v1', restRouter);
+
+// response for GET request when url matches '/'
+// send 'Hello World!' to client nomatter what the request is
+// app.get('/', (req, res) => res.send('Hello World!!!!'));
+
+// launch application, listen on port 3000
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
