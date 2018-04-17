5f96711c3a498fea64f986ce053435da16fd2c92
Xiaohe 小赫 <xiaohehu@users.noreply.github.com>
2018-04-17 17:55:48 -0400

    modified editorSocketService

--- week3/oj-server/services/editorSocketService.js
+++ week3/oj-server/services/editorSocketService.js
@@ -1,5 +1,14 @@
+var redisClient = require('../module/redisClient');
+const TIMEOUT_IN_SECONDS = 3600;
+
 module.exports = function(io) {
-	// collaboration sessions
+
+    // Redis can serve different application
+    // each application has their own session
+    // We also use Redis for our second project
+    var sessionPath = '/temp_session';
+
+	  // collaboration sessions
   	// record all the participants in each session
   	// so that server can send changes to all participants in a session
   	var collaborations = {};
@@ -8,40 +17,39 @@ module.exports = function(io) {
   	var socketIdTSessionId = {};
 
   	io.on('connection', (socket) => {
-    // get sessionId
-    let sessionId = socket.handshake.query['sessionId'];
-    socketIdTSessionId[socket.id] = sessionId;
-    // if sessionId is not in collaborations, it means no one does this problem before
-    if (!(sessionId in collaborations)) {
-      collaborations[sessionId] = {
-        'participants': []
-      };
-    }
+      // get sessionId
+      let sessionId = socket.handshake.query['sessionId'];
+      socketIdTSessionId[socket.id] = sessionId;
+      // if sessionId is not in collaborations, it means no one does this problem before
+      if (!(sessionId in collaborations)) {
+        collaborations[sessionId] = {
+          'participants': []
+        };
+      }
 
-    collaborations[sessionId]['participants'].push(socket.id);
-    // socket event listeners
-    // delta is the change info
-    // it records the row and cloumn of the changes
-    socket.on('change', delta => {
-      // log, easy for debuging
-      console.log("change " + socketIdTSessionId[socket.id] + " " +
-delta);
-      // get session id based on socket.id
-      let sessionId = socketIdTSessionId[socket.id];
-      if (sessionId in collaborations) {
-	        // get all participants on this session
-	        let participants = collaborations[sessionId]['participants'];
-	        // send changes to all participants
-	        for (let i = 0; i < participants.length; i++) {
-	          // skip the one who created this change
-	          if (socket.id != participants[i]) {
-	            io.to(participants[i]).emit("change", delta);
-				} 
-			}
+      collaborations[sessionId]['participants'].push(socket.id);
+      // socket event listeners
+      // delta is the change info
+      // it records the row and cloumn of the changes
+      socket.on('change', delta => {
+        // log, easy for debuging
+        console.log("change " + socketIdTSessionId[socket.id] + " " + delta);
+        // get session id based on socket.id
+        let sessionId = socketIdTSessionId[socket.id];
+        if (sessionId in collaborations) {
+  	        // get all participants on this session
+  	        let participants = collaborations[sessionId]['participants'];
+  	        // send changes to all participants
+  	        for (let i = 0; i < participants.length; i++) {
+  	          // skip the one who created this change
+  	          if (socket.id != participants[i]) {
+  	            io.to(participants[i]).emit("change", delta);
+  				} 
+  			}
 
-      	} else {
-        	console.log("could not tie socket id to any collaboration");
-		} 
-	})
-})
+        	} else {
+          	console.log("could not tie socket id to any collaboration");
+  		} 
+  	})
+  })
 }
